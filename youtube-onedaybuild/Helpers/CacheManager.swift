4db95c1ea68fa8410a82551beffd36a6418b9b6e
hyunsooju <hyunsooting@gmail.com>
2020-10-30 01:17:47 +0900

    added custom tableview cell, configured cell to show video thumbnail, title and data. ALso added cache manager to save image data

---
+++ youtube-onedaybuild/Helpers/CacheManager.swift
@@ -0,0 +1,22 @@
+//
+//  CacheManager.swift
+//  youtube-onedaybuild
+//
+//  Created by hyunsoo Ju on 2020/10/30.
+//
+
+import Foundation
+
+class CacheManager {
+    static var cache = [String:Data]()
+    
+    static func setVideoCache(_ url:String, _ data:Data?) {
+        // Store the image data and use the url as the key
+        cache[url] = data
+    }
+    
+    static func getVideoCache(_ url: String) -> Data? {
+        // Try to get the data for the specified url
+        return cache[url]
+    }
+}
